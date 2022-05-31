da4f2688e3e449ee0792e9558bdd2f41b772294a
Danijel <komsicdanijel@gmail.com>
2022-05-31 16:49:31 -0400

    Added midleware into git

---
+++ API_midleware/HeaderModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_midleware
+{
+    public class HeaderModel
+    {
+        public List<SearchResult> Results { get; set; }
+
+        public class SearchResult
+        {
+            public string Id { get; set; }
+            public string Image { get; set; }
+            public string Title { get; set; }
+        }
+    }
+}
---
+++ API_midleware/YoutubeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_midleware
+{
+    public class YoutubeModel
+    {
+        public string Title { get; set; }
+        public string VideoUrl { get; set; }
+
+    }
+}
